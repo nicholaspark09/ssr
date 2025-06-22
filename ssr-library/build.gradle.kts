e3b2f09754a3615c0fa0bd5408a10f0e4e00ab75
nicholaspark09 <nicholaspark09@gmail.com>
2025-06-22 16:42:23 -0400

    Bump version

--- ssr-library/build.gradle.kts
+++ ssr-library/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate {
 
                 groupId = "com.cincinnatiai"
                 artifactId = "ssr-library"
-                version = "0.0.12"
+                version = "0.0.14"
 
                 pom {
                     name.set("SSR Library")
